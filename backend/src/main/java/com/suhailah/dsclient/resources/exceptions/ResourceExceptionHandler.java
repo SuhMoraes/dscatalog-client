95a7e9684f470e4ec98927745013b9757020e60f
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-06-01 16:58:49 -0300

    Auditing Data

--- backend/src/main/java/com/suhailah/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/suhailah/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.suhailah.dsclient.resources.exceptions;
 
 import java.time.Instant;
 
-import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -10,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.suhailah.dsclient.services.exceptions.ResourceNotFoundExceptions;
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundExceptions.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundExceptions e, HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimeStamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Carolina', '12345678900', 1200.00, TIMESTAMP WITH TIME ZONE '1990-03-22', 2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Alexsandro', '98765432111', 1500.51, TIMESTAMP WITH TIME ZONE '1999-06-16', 5);
-INSERT INTO tb_client (name, cpf, income, birth_Date,  children) VALUES ('Pietro Ferras', '12356479855', 2500.00, TIMESTAMP WITH TIME ZONE '1989-11-15',0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Ana Carolina', '12345678900', 1200.00, TIMESTAMP WITH TIME ZONE '1990-03-22', 2, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Alexsandro', '98765432111', 1500.51, TIMESTAMP WITH TIME ZONE '1999-06-16', 5, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children, created_At) VALUES ('Pietro Ferras', '12356479855', 2500.00, TIMESTAMP WITH TIME ZONE '1989-11-15',0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children, created_At) VALUES ('Lucas Silva e Silva', '98432651200', 10500.00, TIMESTAMP WITH TIME ZONE '1999-12-20',1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children, created_At) VALUES ('Nazareh Tedesco', '10224598744', 3900.00, TIMESTAMP WITH TIME ZONE '1970-01-31',2, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children, created_At) VALUES ('Babalú', '55123441788', 1300.00, TIMESTAMP WITH TIME ZONE '1988-02-27', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children, created_At) VALUES ('Chiquinho', '10325114420', 1000.00, TIMESTAMP WITH TIME ZONE '1979-03-24',9, NOW());
