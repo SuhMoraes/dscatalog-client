6cb8f25ff6a86a039d847d5eefcefe279d0603c4
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-06-01 00:48:52 -0300

    Find Client by id, exception handler

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_client (name, cpf, income, children) VALUES ('Ana Carolina', '12345678900', 1200.00, 2);
-INSERT INTO tb_client (name, cpf, income,  children) VALUES ('Alexsandro', '98765432111', 1500.51, 5);
-INSERT INTO tb_client (name, cpf, income,  children) VALUES ('Pietro Ferras', '12356479855', 2500.00, 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Carolina', '12345678900', 1200.00, TIMESTAMP WITH TIME ZONE '1990-03-22', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Alexsandro', '98765432111', 1500.51, TIMESTAMP WITH TIME ZONE '1999-06-16', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date,  children) VALUES ('Pietro Ferras', '12356479855', 2500.00, TIMESTAMP WITH TIME ZONE '1989-11-15',0);
